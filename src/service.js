ffb66f7400c733fa7ed93c53993b5020344f1dca
tmWebApi <tami.malky.webapi@gmail.com>
2024-01-17 12:21:09 +0200

    Add project files.

---
+++ src/service.js
@@ -0,0 +1,37 @@
+import axios from 'axios';
+
+axios.defaults.baseURL = "https://localhost:7097";
+axios.interceptors.response.use(
+  response => response,
+  error => {
+    if (error.response) {
+      console.log(error.response.data);
+    } else {
+      console.log(error.message);
+    }
+    return Promise.reject(error);
+  }
+);
+export default {
+  getTasks: async () => {
+    const result = await axios.get(`/items`)
+    return result.data;
+  },
+
+  addTask: async (name) => {
+    const result = await axios.post(`/items`, { "name": name , "isComplete": false })
+    return result;
+  },
+
+  setCompleted: async (id, isComplete) => {
+    const result = await axios.put(`/items/${id}/status`, { "isComplete": isComplete });
+    return result;
+  },
+
+  deleteTask: async (id) => {
+    console.log('deleteTask')
+    const result = await axios.delete(`/items/${id}`);
+    console.log(result);
+    return result;
+  }
+};
